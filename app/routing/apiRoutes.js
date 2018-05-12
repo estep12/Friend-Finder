4a66f081a06216092967d4e0b0f6d3845ce89412
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-05-12 18:20:04 -0500

    Function comparing total score difference created

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,32 @@ app.get("/api/friends", function(req, res){
 app.post("/api/friends", function(req, res){
     var userData = req.body;
     console.log(userData);
+    var bestMatch = {
+        name: "",
+        photo: "",
+        friendDiff: 1000
+    }
+    
+    console.log(req.body);
+    
+    var friendScore = userData.scores
+    
+    
+    
+    for(i = 0; i<userData.length; i++){
+        console.log(friendsData[i]);
+        
+        var totalDiff = 0;
+        for(j=0; j<userData.scores.length; j++){
+            totalDiff += Math.abs(parseInt(friendScore[i]) - parseInt(userData[i].scores[j]));
+    
+            if(totalDiff <= bestMatch.friendDiff){
+                bestMatch.name = userData[i].name,
+                bestMatch.photo = userData[i].photo,
+                bestMatch.friendDiff = totalDiff;
+            }
+        }
+    }
 
 
 
